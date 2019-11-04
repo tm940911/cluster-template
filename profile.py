dde87b9caef4402298d4b7041e337b9d34b51da6
Linh B Ngo <lngo@wcupa.edu>
2019-11-04 10:11:19 -0500

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,71 +1,77 @@
-# Import the Portal object.
 import geni.portal as portal
-# Import the ProtoGENI library.
-import geni.rspec.pg as pg
-import geni.rspec.igext as IG
+import geni.rspec.pg as RSpec
+import geni.rspec.igext
 
-# Create a portal context.
 pc = portal.Context()
 
-# Create a Request object to start building the RSpec.
-request = pc.makeRequestRSpec()
+pc.defineParameter( "n", "Number of worker nodes",
+		    portal.ParameterType.INTEGER, 3 )
 
+pc.defineParameter( "raw", "Use physical nodes",
+                    portal.ParameterType.BOOLEAN, False )
 
-tourDescription = \
-"""
-This profile provides the template for a full research cluster with head node, scheduler, compute nodes, and shared file systems.
-First node (head) should contain: 
-- Shared home directory using Networked File System
-- Management server for SLURM
-Second node (metadata) should contain:
-- Metadata server for SLURM
-Third node (storage):
-- Shared software directory (/software) using Networked File System
-Remaining three nodes (computing):
-- Compute nodes  
-"""
+pc.defineParameter( "mem", "Memory per VM",
+		    portal.ParameterType.INTEGER, 256 )
 
-#
-# Setup the Tour info with the above description and instructions.
-#  
-tour = IG.Tour()
-tour.Description(IG.Tour.TEXT,tourDescription)
-request.addTour(tour)
+params = pc.bindParameters()
 
-prefixForIP = "192.168.1."
+IMAGE = "urn:publicid:IDN+emulab.net+image+emulab-ops//hadoop-273"
+SETUP = "http://www.emulab.net/downloads/hadoop-2.7.3-setup.tar.gz"
 
-link = request.LAN("lan")
+def Node( name, public ):
+    if params.raw:
+        return RSpec.RawPC( name )
+    else:
+        vm = geni.rspec.igext.XenVM( name )
+        vm.ram = params.mem
+        if public:
+            vm.routable_control_ip = True
+        return vm
 
-for i in range(6):
-  if i == 0:
-    node = request.XenVM("head")
-    node.routable_control_ip = "true"
-  elif i == 1:
-    node = request.XenVM("metadata")
-  elif i == 2:
-    node = request.XenVM("storage")
-  else:
-    node = request.XenVM("compute-" + str(i-2))
-    node.cores = 4
-    node.ram = 4096
-    
-  node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:CENTOS7-64-STD"
-  
-  iface = node.addInterface("if" + str(i))
-  iface.component_id = "eth1"
-  iface.addAddress(pg.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
-  link.addInterface(iface)
-  
-  node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/passwordless.sh"))
-  node.addService(pg.Execute(shell="sh", command="sudo /local/repository/passwordless.sh"))
-  node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/install_mpi.sh"))
-  node.addService(pg.Execute(shell="sh", command="sudo /local/repository/install_mpi.sh"))
-  
-  # This code segment is added per Benjamin Walker's solution to address the StrictHostKeyCheck issue of ssh
-  # node.addService(pg.Execute(shell="sh", command="sudo chmod 755 /local/repository/ssh_setup.sh"))
-  # node.addService(pg.Execute(shell="sh", command="sudo -H -u lngo bash -c '/local/repository/ssh_setup.sh'"))
- 
-  node.addService(pg.Execute(shell="sh", command="sudo su lngo -c 'cp /local/repository/source/* /users/lngo'"))
-  
-# Print the RSpec to the enclosing page.
-pc.printRequestRSpec(request)
+rspec = RSpec.Request()
+
+lan = RSpec.LAN()
+rspec.addResource( lan )
+
+node = Node( "namenode", True )
+node.disk_image = IMAGE
+node.addService( RSpec.Install( SETUP, "/tmp" ) )
+node.addService( RSpec.Execute( "sh", "sudo /tmp/setup/hadoop-setup.sh" ) )
+node.addService( RSpec.Execute( "sh", "sudo bash /local/repository/setup_hadoop.sh"))
+node.addService( RSpec.Execute( "sh", "sudo bash /local/repository/create_account.sh"))
+
+iface = node.addInterface( "if0" )
+lan.addInterface( iface )
+rspec.addResource( node )
+
+node = Node( "resourcemanager", True )
+node.disk_image = IMAGE
+node.addService( RSpec.Install( SETUP, "/tmp" ) )
+node.addService( RSpec.Execute( "sh", "sudo /tmp/setup/hadoop-setup.sh" ) )
+node.addService( RSpec.Execute( "sh", "sudo bash /local/repository/setup_hadoop.sh"))
+
+iface = node.addInterface( "if0" )
+lan.addInterface( iface )
+rspec.addResource( node )
+
+for i in range( params.n ):
+    node = Node( "worker-" + str( i ), False )
+    node.disk_image = IMAGE
+    node.addService( RSpec.Install( SETUP, "/tmp" ) )
+    node.addService( RSpec.Execute( "sh", "sudo /tmp/setup/hadoop-setup.sh" ) )
+    node.addService( RSpec.Execute( "sh", "sudo bash /local/repository/setup_hadoop.sh"))
+    #node.addService(rspec.Execute(  "sh", "sudo /usr/local/hadoop-2.7.3/bin/hdfs --daemon start datanode"))
+    #node.addService(rspec.Execute(  "sh", "sudo /usr/local/hadoop-2.7.3/bin/hdfs --daemon start nodemanager"))
+
+    iface = node.addInterface( "if0" )
+    lan.addInterface( iface )
+    rspec.addResource( node )
+
+from lxml import etree as ET
+
+tour = geni.rspec.igext.Tour()
+tour.Description( geni.rspec.igext.Tour.TEXT, "A cluster running Hadoop 2.7.3. It includes a name node, a resource manager, and as many workers as you choose." )
+tour.Instructions( geni.rspec.igext.Tour.MARKDOWN, "After your instance boots (approx. 5-10 minutes), you can log into the resource manager node and submit jobs.  [The HDFS web UI](http://{host-namenode}:50070/) and [the resource manager UI](http://{host-resourcemanager}:8088/) will also become available." )
+rspec.addTour( tour )
+
+pc.printRequestRSpec( rspec )
